8c90debd9f6c95addd9ff2c032c5e7b6bb55ce24
Michal <mkowalczewskisda@gmail.com>
2019-11-29 19:58:59 +0100

    WIP - Implemented basic logic into Main class - TODO implement few animals

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import model.Animal;
+
 public class Main {
     public static void main(String[] args) {
-
+        //TODO implement few animals to use them as examples
+        Animal animal;
+        String predator = animal.isPredator() ? "drapieżnik":"roślinożerca";
+        String fly = animal.canFly() ? "potrafi":"nie potrafi";
+        System.out.println("Nasze zwierze to: " +animal.getSpecies()+". Jest to " + predator +" i " +fly+" latać");
     }
 }
