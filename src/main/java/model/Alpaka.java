43947d63bd9181a980612b954bc31d422cdb111a
Bartlomiej Jablonowski <baiokai@gmail.com>
2019-11-30 12:20:24 +0100

    alpaka

--- src/main/java/model/Alpaka.java
+++ src/main/java/model/Alpaka.java
@@ -1,4 +1,22 @@
 package model;
 
-public class Alpaka {
+public class Alpaka implements Animal {
+    String spice;
+    Boolean isPredator;
+    Boolean canFly;
+    
+    @Override
+    public String getSpecies() {
+        return "Alpaka";
+    }
+
+    @Override
+    public boolean isPredator() {
+        return false;
+    }
+
+    @Override
+    public boolean canFly() {
+        return false;
+    }
 }
