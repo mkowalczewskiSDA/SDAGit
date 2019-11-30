9d6db4a409109f789cb58dc1f48884549b2bd80f
Jareczeq <contact@jareczeq.pl>
2019-11-30 12:33:35 +0100

    dodano nowego animala

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import model.Animal;
+import model.Krokodyl;
 
 public class Main {
     public static void main(String[] args) {
         //TODO implement few animals to use them as examples
-        Animal animal;
+        Animal animal = new Krokodyl("Krokodyl", true, false);
         String predator = animal.isPredator() ? "drapieżnik":"roślinożerca";
         String fly = animal.canFly() ? "potrafi":"nie potrafi";
         System.out.println("Nasze zwierze to: " +animal.getSpecies()+". Jest to " + predator +" i " +fly+" latać");
