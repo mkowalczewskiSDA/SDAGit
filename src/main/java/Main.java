8a71b209a7d7df7edca396680250b5c76b30f9bd
Norbert <syska@gmail.com>
2019-11-30 12:12:58 +0100

    dodalem zwierze koala

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,22 @@ import model.Animal;
 public class Main {
     public static void main(String[] args) {
         //TODO implement few animals to use them as examples
-        Animal animal;
+        Animal animal = new Animal() {
+            @Override
+            public String getSpecies() {
+                return "Koala";
+            }
+
+            @Override
+            public boolean isPredator() {
+                return false;
+            }
+
+            @Override
+            public boolean canFly() {
+                return false;
+            }
+        };
         String predator = animal.isPredator() ? "drapieżnik":"roślinożerca";
         String fly = animal.canFly() ? "potrafi":"nie potrafi";
         System.out.println("Nasze zwierze to: " +animal.getSpecies()+". Jest to " + predator +" i " +fly+" latać");
