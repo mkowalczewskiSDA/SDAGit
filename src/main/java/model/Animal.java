6baf30279b5ced9fddb6de3081ee4b4bab12c3b4
Radek <radek.halfaf@gmail.com>
2019-11-30 12:20:23 +0100

    kolejny commit

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -4,8 +4,6 @@ public interface Animal {
     public String getSpecies();
     public boolean isPredator();
     public boolean canFly();
-    public String type;
-    public int weight;
-    public String name;
-    
+
+
 }
