a0077abbf80512c57303b04f162f142e085fdecf
Bartlomiej Jablonowski <baiokai@gmail.com>
2019-11-30 12:35:24 +0100

    AlpakaCoDzialaISkacze

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,11 @@
+import model.Alpaka;
 import model.Animal;
 
 public class Main {
     public static void main(String[] args) {
         //TODO implement few animals to use them as examples
-<<<<<<< HEAD
-        Animal  animal;
+        Animal animal = new Alpaka("Alpaka", false, false);
 
-
-=======
-        Animal animal = new Animal() {
-            @Override
-            public String getSpecies() {
-                return "Koala";
-            }
-
-            @Override
-            public boolean isPredator() {
-                return false;
-            }
-
-            @Override
-            public boolean canFly() {
-                return false;
-            }
-        };
->>>>>>> 900e93dee8ff37759c89db8000dc6709c0054029
         String predator = animal.isPredator() ? "drapieżnik":"roślinożerca";
         String fly = animal.canFly() ? "potrafi":"nie potrafi";
         System.out.println("Nasze zwierze to: " +animal.getSpecies()+". Jest to " + predator +" i " +fly+" latać");
--- src/main/java/model/Alpaka.java
+++ src/main/java/model/Alpaka.java
@@ -5,18 +5,24 @@ public class Alpaka implements Animal {
     Boolean isPredator;
     Boolean canFly;
 
+    public Alpaka(String spice, Boolean isPredator, Boolean canFly) {
+        this.spice = spice;
+        this.isPredator = isPredator;
+        this.canFly = canFly;
+    }
+
     @Override
     public String getSpecies() {
-        return "Alpaka";
+        return spice;
     }
 
     @Override
     public boolean isPredator() {
-        return false;
+        return isPredator;
     }
 
     @Override
     public boolean canFly() {
-        return false;
+        return canFly;
     }
 }
