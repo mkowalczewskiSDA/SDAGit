9f343c34b412407b6a884f0e61fa3fd792d742ce
Norbert <syska@gmail.com>
2019-11-30 12:50:58 +0100

    dodalem koale

--- src/main/java/model/Koala.java
+++ src/main/java/model/Koala.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Koala {
+public class Koala implements Animal{
+
+
+    @Override
+    public String getSpecies() {
+        return "Koala";
+    }
+
+    @Override
+    public boolean isPredator() {
+        return false;
+    }
+
+    @Override
+    public boolean canFly() {
+        return false;
+    }
 }
