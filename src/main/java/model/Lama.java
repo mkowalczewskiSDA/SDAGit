35fba6e9c4c23748adee96529a2066f219f354c7
Bartlomiej Jablonowski <baiokai@gmail.com>
2019-11-30 12:43:12 +0100

    Lama2

---
+++ src/main/java/model/Lama.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Lama implements Animal {
+    String
+    @java.lang.Override
+    public String getSpecies() {
+        return null;
+    }
+
+    @java.lang.Override
+    public boolean isPredator() {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean canFly() {
+        return false;
+    }
+}
