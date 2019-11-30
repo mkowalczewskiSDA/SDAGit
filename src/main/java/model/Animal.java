19b3b9270e24f172fc37eff39f8af37febb12f43
Radek <radek.halfaf@gmail.com>
2019-11-30 12:16:13 +0100

    implementacja animal

--- src/main/java/model/Animal.java
+++ src/main/java/model/Animal.java
@@ -4,4 +4,8 @@ public interface Animal {
     public String getSpecies();
     public boolean isPredator();
     public boolean canFly();
+    public String type;
+    public int weight;
+    public String name;
+    
 }
