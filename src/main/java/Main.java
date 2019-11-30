938f8957e92a3170ab0e791d3f8d32419ad744b9
Michal <mkowalczewskisda@gmail.com>
2019-11-30 13:43:05 +0100

    Revert "dodano nowego animala"
    This reverts commit 9d6db4a409109f789cb58dc1f48884549b2bd80f.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 import model.Animal;
-import model.Krokodyl;
 
 public class Main {
     public static void main(String[] args) {
         //TODO implement few animals to use them as examples
-        Animal animal = new Krokodyl("Krokodyl", true, false);
+        Animal animal;
         String predator = animal.isPredator() ? "drapieżnik":"roślinożerca";
         String fly = animal.canFly() ? "potrafi":"nie potrafi";
         System.out.println("Nasze zwierze to: " +animal.getSpecies()+". Jest to " + predator +" i " +fly+" latać");
